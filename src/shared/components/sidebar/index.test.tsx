dc67ca6b04bf070a74682c9f5a42051df62ddda0
Nurmuhammad032 <nurmuhammad2003o@gmail.com>
2024-02-20 18:33:24 +0500

    fix sidebar test error

--- src/shared/components/sidebar/index.test.tsx
+++ src/shared/components/sidebar/index.test.tsx
@@ -1,23 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import ProjectSidebar from './index';
-import { navLinks } from '@/shared/constants/navLinks';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react'
+import ProjectSidebar from './index'
+import { navLinks } from '@/shared/constants/nav-links'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('ProjectSidebar', () => {
   test('renders logo, logo text, project description, and navigation links', () => {
-    render(<ProjectSidebar />, { wrapper: BrowserRouter });
+    render(<ProjectSidebar />, { wrapper: BrowserRouter })
 
-    const logoElement = screen.getByAltText(/logo/i);
-    expect(logoElement).toBeInTheDocument();
+    const logoElement = screen.getByAltText(/logo/i)
+    expect(logoElement).toBeInTheDocument()
 
-    const logoTextElement = screen.getByText('Finance Peak');
-    expect(logoTextElement).toBeInTheDocument();
+    const logoTextElement = screen.getByText('Finance Peak')
+    expect(logoTextElement).toBeInTheDocument()
 
-    const projectDescriptionElement = screen.getByText('Strategic Money Moves');
-    expect(projectDescriptionElement).toBeInTheDocument();
+    const projectDescriptionElement = screen.getByText('Strategic Money Moves')
+    expect(projectDescriptionElement).toBeInTheDocument()
 
     navLinks.forEach(({ label, path }) => {
-      expect(screen.getByRole('link', { name: label })).toHaveAttribute('href', path);
-    });
-  });
-});
+      expect(screen.getByRole('link', { name: label })).toHaveAttribute('href', path)
+    })
+  })
+})
